d441d78711bd8dbf1e67fa5338d88bd3e9866576
JaeHyun <soehd0889@naver.com>
2021-05-30 23:42:15 +0900

    TypeScript 와 Context API 활용하기

--- ts-react-tutorial/src/App.tsx
+++ ts-react-tutorial/src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReducerSample from './component/ReducerSample';
+import { SampleProvider } from './component/SampleContext';
 
 const App: React.FC = () => {
-  return <ReducerSample />;
+  return (
+    <SampleProvider>
+      <ReducerSample />
+    </SampleProvider>
+  );
 };
 
 export default App;
