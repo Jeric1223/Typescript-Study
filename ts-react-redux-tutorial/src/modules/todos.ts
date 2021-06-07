df1c510f7953521b5e7b9db94ae68713c8eaf9b6
JaeHyun <soehd0889@naver.com>
2021-06-07 22:17:23 +0900

    TypeScript

--- ts-react-redux-tutorial/src/modules/todos.ts
+++ ts-react-redux-tutorial/src/modules/todos.ts
@@ -5,7 +5,7 @@ const REMOVE_TODO = 'todos/REMOVE_TODO' as const;
 
 let nextId = 1; // 새로운 항목을 추가 할 때 사용 할 고유 ID 값
 
-// 액션 생성 함수
+// 액션 생성 함수.
 export const addTodo = (text: string) => ({
   type: ADD_TODO,
   payload: {
